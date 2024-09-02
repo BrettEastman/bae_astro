a4a402c0bad1e1be8923a3f7ffd97ed52c3b3975
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2024-09-01 19:57:33 -0700

    Implement Vercel analytics - install dependency and modify the astro config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
+import vercel from "@astrojs/vercel/serverless";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.brettaustineastman.com/",
   integrations: [tailwind(), react(), icon()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });
