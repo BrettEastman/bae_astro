d7047a1cff935ba6df059e753d57ba3a1d33c919
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2024-09-01 19:14:16 -0700

    Add Astro icon and iconofy dependencies. Refactored and reconfigured icons in the footer. Add Social component and update styles in global.css.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.brettaustineastman.com/",
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), icon()],
 });
