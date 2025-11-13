d48cfd04a4a74b0472d5e558a26cabacd811dabf
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2025-11-12 17:25:57 -0800

    Add new items to data with all correct links and test price ids - all functioning well in test mode

--- src/data/products.js
+++ src/data/products.js
@@ -1,20 +1,47 @@
 export const products = [
   {
     id: "drum-book",
-    name: "Internal/External",
-    description: "Contemporary music score",
-    priceId: "price_1SSn4XPjBsaDuHZoEqiqgAHv",
-    price: 10.0, // for display purposes
+    name: "Patterns and Beats",
+    description: "Educational drum book",
+    priceId: "price_1SSoaBPjBsaDuHZo7qyqXENh",
+    price: 10.0,
     downloadUrl:
-      "https://drive.google.com/file/d/1MRRoPEmUf1chrqRHbMwg3b_8IYUsvMip/view",
+      "https://drive.google.com/file/d/1wiL7xMUoURsQ0qE2VevgQaO9fwSChHC4/view?usp=sharing",
   },
   {
     id: "score-1",
-    name: "Paraphrase",
+    name: "Internal/External",
     description: "Contemporary music score",
     priceId: "price_1SSn5NPjBsaDuHZoEoIOKGFN",
     price: 10.0,
     downloadUrl:
-      "https://drive.google.com/file/d/1MRRoPEmUf1chrqRHbMwg3b_8IYUsvMip/view",
+      "https://drive.google.com/file/d/1MRRoPEmUf1chrqRHbMwg3b_8IYUsvMip/view?usp=sharing",
+  },
+  {
+    id: "score-2",
+    name: "Paraphrase",
+    description: "Contemporary music score",
+    priceId: "price_1SSn4XPjBsaDuHZoEqiqgAHv",
+    price: 10.0,
+    downloadUrl:
+      "https://drive.google.com/file/d/153UqdWHacSAUoLJeYok7_x3tvq8JM7Cu/view?usp=sharing",
+  },
+  {
+    id: "score-3",
+    name: "La Musique",
+    description: "Contemporary music score",
+    priceId: "price_1SSoh9PjBsaDuHZo51J98t6g",
+    price: 10.0,
+    downloadUrl:
+      "https://drive.google.com/file/d/1Mj7NvM81wbgKpZba8Ux07-9anjkErJdE/view?usp=sharing",
+  },
+  {
+    id: "score-4",
+    name: "MAINFRAME",
+    description: "Contemporary music score",
+    priceId: "price_1SSoeZPjBsaDuHZoYV7fJWVf",
+    price: 10.0,
+    downloadUrl:
+      "https://drive.google.com/file/d/13yk9i5ab_1MlWm6rPimtz4pfo_7JSz34/view?usp=sharing",
   },
 ];
