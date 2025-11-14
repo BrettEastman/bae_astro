e3420da69cb654c96f98c864da31fbc154e0ceed
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2025-11-14 13:03:08 -0800

    Add three new products with all data - in response, internal external, keepsake, misma

--- src/data/products.production.js
+++ src/data/products.production.js
@@ -2,8 +2,7 @@ export const products = [
   {
     id: "drum-book",
     name: "Patterns and Beats",
-    description:
-      "Patterns and Beats is an incredibly useful drum book for the 21st century drum student. This book contains a collection of linear patterns and a curated selection of enjoyable and educational drum beats in standard notation, arranged by difficulty, and all aesthetically designed for readability, clarity, and comfort.",
+    description: `Patterns and Beats is an incredibly useful drum book for the 21st century drum student. This book contains a collection of linear patterns and a curated selection of enjoyable and educational drum beats in standard notation, arranged by difficulty, and all aesthetically designed for readability, clarity, and comfort.`,
     priceId: "price_1SSmJ6PjBsaDuHZoFLi0icQ2",
     price: 10.0,
     image: "/products/patterns-and-beats.jpg",
@@ -23,8 +22,7 @@ export const products = [
   {
     id: "score-2",
     name: "Paraphrase",
-    description:
-      "Paraphrase for viola, bass clarinet, and piano unfolds in three movements, each a five-minute exploration of connected themes and bold contrasts. Drawing on sources from Liszt to 90s sludge-core band Noothgrush—and echoes of the composer's own works—it reimagines and expands its inspirations. The trio performs with the intensity and freedom of a rock band, embracing space, distortion, and expression.",
+    description: `Paraphrase for viola, bass clarinet, and piano unfolds in three movements, each a five-minute exploration of connected themes and bold contrasts. Drawing on sources from Liszt to 90s sludge-core band Noothgrush—and echoes of the composer's own works—it reimagines and expands its inspirations. The trio performs with the intensity and freedom of a rock band, embracing space, distortion, and expression.`,
     priceId: "price_1SSmozPjBsaDuHZotw3jl3pP",
     price: 10.0,
     image: "/products/paraphrase.jpg",
@@ -34,8 +32,7 @@ export const products = [
   {
     id: "score-3",
     name: "La Musique",
-    description:
-      "LA MUSIQUE for flute and alto flute draws from Charles Baudelaire's poem of the same name. Using a cryptogram that translates French text into pitch, rhythm, and aspirated sound, the work transforms language into music. Layered with melodic renditions of the spoken poem and a recurring lyrical theme, it mirrors the poem's structure through shifting textures, timbres, and parallel musical form.",
+    description: `LA MUSIQUE for flute and alto flute draws from Charles Baudelaire's poem of the same name. Using a cryptogram that translates French text into pitch, rhythm, and aspirated sound, the work transforms language into music. Layered with melodic renditions of the spoken poem and a recurring lyrical theme, it mirrors the poem's structure through shifting textures, timbres, and parallel musical form.`,
     priceId: "price_1SSrXqPjBsaDuHZoBLDiAO1q",
     price: 10.0,
     image: "/products/la-musique.jpg",
@@ -45,8 +42,7 @@ export const products = [
   {
     id: "score-4",
     name: "MAINFRAME",
-    description:
-      "MAINFRAME for string quartet explores distorted symmetry and the evolution or decay of reflection. Across seven movements and 23 mosaic-like sections (11+1+11), moments recur in altered or reversed forms—sometimes recognizable, sometimes not. Centered on an 11-tone sequence, the work's unity of form, rhythm, and pitch creates a precise yet shifting architecture of sound and memory.",
+    description: `MAINFRAME for string quartet explores distorted symmetry and the evolution or decay of reflection. Across seven movements and 23 mosaic-like sections (11+1+11), moments recur in altered or reversed forms—sometimes recognizable, sometimes not. Centered on an 11-tone sequence, the work's unity of form, rhythm, and pitch creates a precise yet shifting architecture of sound and memory.`,
     priceId: "price_1SSrXFPjBsaDuHZomNWHDZ5v",
     price: 10.0,
     image: "/products/mainframe.jpg",
@@ -56,12 +52,41 @@ export const products = [
   {
     id: "score-5",
     name: "Passover",
-    description:
-      "PASSOVER for solo flute explores feedback as both concept and process. Based on a poem about anxiety and self-calming, the text is transformed into music through cryptographic means, then reinterpreted by the poet-performer. Each layer—composer, performer, audience—forms a loop of reflection, where meaning dissolves and re-emerges through sound.",
+    description: `PASSOVER for solo flute explores feedback as both concept and process. Based on a poem about anxiety and self-calming, the text is transformed into music through cryptographic means, then reinterpreted by the poet-performer. Each layer—composer, performer, audience—forms a loop of reflection, where meaning dissolves and re-emerges through sound.`,
     priceId: "price_1SSrWUPjBsaDuHZoGPjqL5OL",
     price: 10.0,
     image: "/products/passover.jpg",
     downloadUrl:
-      "https://drive.google.com/file/d/12ygPHCzmbl4aT7BpYVQOv9TIcrlbDy0Y/view?usp=sharing",
+      "https://drive.google.com/file/d/1MxVEEOtS2vqhFyn_TeFAKWLP6ARsMRT5/view?usp=sharing",
+  },
+  {
+    id: "score-6",
+    name: "Keepsake (for Rishi)",
+    description: `Keepsake for contrabass flute and contraforte honors Rishi Dhanivan Sweeney, the daughter of a close friend who passed away at two months. The piece weaves themes from rock songs the composer and Rishi’s father once played together with cryptogram-based transcriptions of quotations addressed to her. These contrasting musical languages form a mosaic reflecting memory, grief, and a metaphysical call to a life gone too soon.`,
+    priceId: "price_1STT6yPjBsaDuHZoG9dIOkAt",
+    price: 0.0,
+    image: "/products/keepsake.jpg",
+    downloadUrl:
+      "https://drive.google.com/file/d/1v0dKMKkPZlkkLv-gaKbP98nNVx4Ub1Fv/view?usp=sharing",
+  },
+  {
+    id: "score-7",
+    name: "In Response",
+    description: `In Response for flute and fixed media reflects the struggle of confronting society's relentless narratives without a way to speak back. The fixed media—an electric-guitar feedback performance—symbolizes overwhelming information from distant sources, while the flute embodies the individual voice pushing against it, offering a fragile but insistent response.`,
+    priceId: "price_1STSrcPjBsaDuHZoAjelJexU",
+    price: 10.0,
+    image: "/products/in-response.jpg",
+    downloadUrl:
+      "https://drive.google.com/file/d/1NUGxu3U5q11sKJXhS-QQ7Z8o743kM0QN/view?usp=sharing",
+  },
+  {
+    id: "score-8",
+    name: "MISMA",
+    description: `MISMA for violin, clarinet, cello, and piano unfolds in five movements, each built from a quote by an artist meaningful to the composer—Hildegard von Bingen, Solange, Amber Sermeño, Ara Jo, Etta James, Kathleen Hanna, and Naomi Stine. Using a cryptogram inspired by Messiaen’s “language of the angels,” the work transforms text into musical material. Named for the Spanish word for “same,” MISMA reflects fractal ideas, altered symmetry, and the voices that shaped its creation.`,
+    priceId: "price_1STSp3PjBsaDuHZocylrEZWJ",
+    price: 10.0,
+    image: "/products/misma.jpg",
+    downloadUrl:
+      "https://drive.google.com/file/d/1B5Dx9SGDUWW82eLma1XTIafrohNHMNi5/view?usp=sharing",
   },
 ];
