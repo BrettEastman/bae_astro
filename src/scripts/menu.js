f65a785c11c2fabd89ae2eff8bb1094ee4437dc9
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2026-07-10 15:44:00 -0700

    fix: make hamburger menu accessible button with aria-expanded/controls

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -2,5 +2,7 @@ document.querySelector(".hamburger").addEventListener("click", () => {
   document.querySelectorAll(".nav-links").forEach((nav) => {
     nav.classList.toggle("expanded");
   });
-  document.querySelector(".hamburger").classList.toggle("active");
+  const hamburger = document.querySelector(".hamburger");
+  const isExpanded = hamburger.classList.toggle("active");
+  hamburger.setAttribute("aria-expanded", String(isExpanded));
 });
